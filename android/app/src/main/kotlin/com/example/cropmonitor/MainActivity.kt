07938be0311ab2e0b49a146592030f325942e3e9
oatanurakch <oatty6106164@gmail.com>
2021-11-05 17:11:05 +0700

    add first file

---
+++ android/app/src/main/kotlin/com/example/cropmonitor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cropmonitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Croptye.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class ChooseCropType extends StatefulWidget {
+  const ChooseCropType({Key? key}) : super(key: key);
+
+  @override
+  _ChooseCropTypeState createState() => _ChooseCropTypeState();
+}
+
+class _ChooseCropTypeState extends State<ChooseCropType> {
+  List items = [
+    {
+      'title': 'สวนอินทผาลัม',
+      'subtitle': 'อัพเดตเมื่อ 05/11/2021 17:00',
+    },
+    {
+      'title': 'สวนองุ่น',
+      'subtitle': 'อัพเดตเมื่อ 05/11/2021 17:00',
+    },
+    {
+      'title': 'สวนกล้วย',
+      'subtitle': 'อัพเดตเมื่อ 05/11/2021 17:00',
+    },
+    {
+      'title': 'สวนแก้วมังกร',
+      'subtitle': 'อัพเดตเมื่อ 05/11/2021 17:00',
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Crop'),
+        centerTitle: true,
+      ),
+      body: Container(
+        // MediaQuery ใช้ปรับความกว้างแอพให้เท่าจอ
+        width: MediaQuery.of(context).size.width,
+        height: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'เลือกแปลงการเกษตรของคุณ',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: InkWell(
+                        onTap: () {print('Inkwell');},
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black54,
+                              ),
+                            ],
+                          ),
+                          width: MediaQuery.of(context).size.width,
+                          height: 70,
+                          margin: EdgeInsets.only(
+                            bottom: 20,
+                          ),
+                          child: ListTile(
+                            title: Text(
+                              '${items[index]['title']}',
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            subtitle: Text(
+                              '${items[index]['subtitle']}',
+                            ),
+                            trailing: GestureDetector(
+                              child: Icon(Icons.mode_edit_rounded),
+                              onTap: (){print('edit');},
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
