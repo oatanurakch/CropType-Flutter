d8b19d30099c9af285fe98dfb40de3c5f96d5853
oatanurakch <oatty6106164@gmail.com>
2021-11-09 15:48:52 +0700

    edit monitor file

--- lib/monitorcrop.dart
+++ lib/monitorcrop.dart
@@ -11,6 +11,8 @@ class buildCropMonitor extends StatefulWidget {
 
 class _buildCropMonitorState extends State<buildCropMonitor> {
   var _title;
+  double w_containnerinshow = 180;
+  double h_containnerinshow = 180;
   @override
   void initState() {
     // TODO: implement initState
@@ -26,11 +28,224 @@ class _buildCropMonitorState extends State<buildCropMonitor> {
         centerTitle: true,
       ),
       body: Container(
+        color: Colors.grey.shade300,
         width: MediaQuery.of(context).size.width,
         height: double.infinity,
-        child: Column(
-          
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 5,
+            left: 5,
+            right: 5,
+          ),
+          child: SingleChildScrollView(
+            child: Row(
+              children: [
+                Spacer(),
+                Column(
+                  children: [build_Data(1, 30, 36.75, 89, 90.12)],
+                ),
+                Spacer(),
+                Column(
+                  children: [build_Data(2, 29.98, 36.80, 89.12, 89.89)],
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget build_Data(titleindex, temp, soiltemp, humid, soilmois) {
+    return Container(
+      width: w_containnerinshow,
+      height: h_containnerinshow,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black54,
+          ),
+        ],
+        color: Colors.white,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'อินทผาลัม ต้นที่ $titleindex',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          Container(
+            height: 0.5,
+            width: w_containnerinshow,
+            color: Colors.black,
+          ),
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    width: w_containnerinshow / 2,
+                    height: h_containnerinshow - 35,
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Temp.',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '$temp ˚C',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Soil Temp.',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '$soiltemp ˚C',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    width: w_containnerinshow / 2,
+                    height: h_containnerinshow - 35,
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Humid',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '$humid %',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Soil Mois.',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '$soilmois %',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
